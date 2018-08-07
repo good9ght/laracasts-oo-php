7599aa93900a7be3150c93cfdc28a535ed1aeda6
Victor <victor.ananias8@gmail.com>
2018-08-07 20:35:01 -0300

    save

--- index.php
+++ index.php
@@ -1,47 +1,10 @@
 <?php
 
-class Person {
+require "vendor/autoload.php";
 
-    protected $name;
-
-    public function __construct($name) {
-        $this->name = $name;
-    }
-}
-
-class Business {
-
-    protected $staff;
-
-    public function __construct(Staff $staff) {
-        $this->staff = $staff;
-    }
-
-    public function hire(Person $person) {
-        $this->staff->add($person);
-    }
-
-    public function getStaffMembers() {
-        return $this->staff->members();
-    }
-
-}
-
-class Staff {
-    protected $members = [];
-
-    public function __construct($members) {
-        $this->members = $members;
-    }
-
-    public function add(Person $person) {
-        $this->members[] = $person;
-    }
-
-    public function members() {
-        return $this->members;
-    }
-}
+use App\Users\Person;
+use App\Staff;
+use App\Business;
 
 $victor = new Person("Victor");
 
